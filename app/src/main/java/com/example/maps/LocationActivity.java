ccee67cb3cf26639bea0fb25c714a1daa3e4e0c6
abdul <abdul.tanveer@gmail.com>
2019-07-18 08:22:57 +0530

    location address service

--- app/src/main/java/com/example/maps/LocationActivity.java
+++ app/src/main/java/com/example/maps/LocationActivity.java
@@ -6,10 +6,15 @@ import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.ResultReceiver;
 import android.util.Log;
+import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
@@ -21,6 +26,7 @@ import java.util.Locale;
 
 public class LocationActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getSimpleName();
+    private AddressResultReceiver mResultReceiver;
 
     private static final int REQUEST_PERMISSIONS_REQUEST_CODE = 34;
 
@@ -39,6 +45,46 @@ public class LocationActivity extends AppCompatActivity {
     private TextView mLatitudeText;
     private TextView mLongitudeText;
 
+
+
+
+
+    private static final String ADDRESS_REQUESTED_KEY = "address-request-pending";
+    private static final String LOCATION_ADDRESS_KEY = "location-address";
+
+    /**
+     * Provides access to the Fused Location Provider API.
+     */
+
+    /**
+     * Represents a geographical location.
+     */
+
+    /**
+     * Tracks whether the user has requested an address. Becomes true when the user requests an
+     * address and false when the address (or an error message) is delivered.
+     */
+    private boolean mAddressRequested;
+
+    /**
+     * The formatted location address.
+     */
+    private String mAddressOutput;
+
+    /**
+     * Receiver registered with this activity to get the response from FetchAddressIntentService.
+     */
+
+    /**
+     * Displays the location address.
+     */
+    private TextView mLocationAddressTextView;
+
+    /**
+     * Visible while the address is being fetched.
+     */
+    private ProgressBar mProgressBar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -48,6 +94,10 @@ public class LocationActivity extends AppCompatActivity {
         mLongitudeLabel = getResources().getString(R.string.longitude_label);
         mLatitudeText = (TextView) findViewById((R.id.latitude_text));
         mLongitudeText = (TextView) findViewById((R.id.longitude_text));
+        mLocationAddressTextView = (TextView) findViewById(R.id.textViewPostaladdress);
+
+        mResultReceiver = new AddressResultReceiver(new Handler());
+
 
         mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
     }
@@ -178,5 +228,75 @@ public class LocationActivity extends AppCompatActivity {
     }
 
 
+    public void clickHandler(View view) {
+        if (mLastLocation != null) {
+            startIntentService();
+            return;
+        }
 
+        // If we have not yet retrieved the user location, we process the user's request by setting
+        // mAddressRequested to true. As far as the user is concerned, pressing the Fetch Address button
+        // immediately kicks off the process of getting the address.
+        mAddressRequested = true;
+        updateUIWidgets();    }
+
+    private void startIntentService() {
+        Intent intent = new Intent(this, FetchAddressIntentService.class);
+
+        // Pass the result receiver as an extra to the service.
+        intent.putExtra(Constants.RECEIVER, mResultReceiver);
+
+        // Pass the location data as an extra to the service.
+        intent.putExtra(Constants.LOCATION_DATA_EXTRA, mLastLocation);
+
+        // Start the service. If the service isn't already running, it is instantiated and started
+        // (creating a process for it if needed); if it is running then it remains running. The
+        // service kills itself automatically once all intents are processed.
+        startService(intent);
+    }
+
+
+
+    private class AddressResultReceiver extends ResultReceiver {
+        AddressResultReceiver(Handler handler) {
+            super(handler);
+        }
+
+        /**
+         *  Receives data sent from FetchAddressIntentService and updates the UI in MainActivity.
+         */
+        @Override
+        protected void onReceiveResult(int resultCode, Bundle resultData) {
+
+            // Display the address string or an error message sent from the intent service.
+            mAddressOutput = resultData.getString(Constants.RESULT_DATA_KEY);
+            displayAddressOutput();
+
+            // Show a toast message if an address was found.
+            if (resultCode == Constants.SUCCESS_RESULT) {
+                //showToast(getString(R.string.address_found));
+            }
+
+            // Reset. Enable the Fetch Address button and stop showing the progress bar.
+            mAddressRequested = false;
+//            updateUIWidgets();
+        }
+    }
+
+    private void displayAddressOutput() {
+        mLocationAddressTextView.setText(mAddressOutput);
+    }
+
+    /**
+     * Toggles the visibility of the progress bar. Enables or disables the Fetch Address button.
+     */
+    private void updateUIWidgets() {
+        if (mAddressRequested) {
+            //mProgressBar.setVisibility(ProgressBar.VISIBLE);
+           // mFetchAddressButton.setEnabled(false);
+        } else {
+          //  mProgressBar.setVisibility(ProgressBar.GONE);
+            //mFetchAddressButton.setEnabled(true);
+        }
+    }
 }
